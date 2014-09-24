44566b976abe6555a29bf52182c9610fc432bb08
ポールシード <cb.so.devope+0001@gmail.com>
2014-09-24 19:34:03 +0900

    Create AbstVehicle.java

---
+++ src/jp/com/koto/myItem/vehicle/AbstVehicle.java
@@ -0,0 +1,34 @@
+package vehicle;
+
+public abstract class AbstVehicle implements IVehicle {
+
+    protected Double range = null;
+
+    protected Double speed = null;
+
+    protected Double time = null;
+
+    @Override
+    public void move(Double range, Double speed) {
+        move(range);
+        this.speed = speed;
+    }
+
+    @Override
+    public void move(Double range) {
+        this.range = range;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public Double getRange() {
+        return range;
+    }
+
+    public Double getTime() {
+        return time;
+    }
+
+}
