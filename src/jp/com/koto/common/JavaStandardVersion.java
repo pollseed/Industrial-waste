6f76ca108ec64c63e3ff1287ce2976f4375d6368
ポールシード <cb.so.devope+0001@gmail.com>
2014-09-30 18:13:50 +0900

    :sunglasses: 未実装です

---
+++ src/jp/com/koto/common/JavaStandardVersion.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * TODO 未実装
+ * 
+ * @author pollseed
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JavaStandardVersion {
+
+}
