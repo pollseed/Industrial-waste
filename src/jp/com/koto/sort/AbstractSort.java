148cce0c8b3c293f1a323a2581c76e6d33a246f9
Seed <cb.so.devope+0001@gmail.com>
2014-10-31 16:42:05 +0900

    Create AbstractSort.java

---
+++ src/jp/com/koto/sort/AbstractSort.java
@@ -0,0 +1,38 @@
+package jp.com.argorithm;
+
+public abstract class AbstractSort {
+
+    /* Constant */
+
+    /** Initial value **/
+    protected final int DEFAULT = Integer.MIN_VALUE;
+
+    /** Sentinel **/
+    protected final int SENTINEL = Integer.MAX_VALUE;
+
+    /* Member variable */
+
+    /** Array to be sorted **/
+    protected final int[] searchArray;
+
+    /** Sorted array **/
+    protected int[] sorted;
+
+    /* constructor */
+
+    protected AbstractSort(int[] args) {
+        this.searchArray = args;
+    }
+
+    /**
+     * use the random sequences
+     * @param maxArray Number of arrays
+     * @param maxValue 1～any value
+     */
+    protected AbstractSort(int maxArray, int maxValue) {
+        searchArray = new int[maxArray];
+        for (int i = 0; i < maxArray; i++) {
+            searchArray[i] = (int) (Math.random() * maxValue + 1);
+        }
+    }
+}
