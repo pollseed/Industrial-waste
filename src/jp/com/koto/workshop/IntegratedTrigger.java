dc2933e32ad7b3185f66420e62b01d7c8fdeabb0
Seed <cb.so.devope+0001@gmail.com>
2014-11-06 15:25:59 +0900

    Create IntegratedTrigger.java

---
+++ src/jp/com/koto/workshop/IntegratedTrigger.java
@@ -0,0 +1,15 @@
+package hoge;
+
+/**
+ * 勉強会用テストクラス(超入門編)
+ *
+ */
+final public class IntegratedTrigger {
+
+    public static void main(String[] args) {
+
+        // アクセステスト
+        new AccessStudy().accessSame().accessAnother();
+
+    }
+}
