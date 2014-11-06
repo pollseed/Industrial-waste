11c0605bf13ffa73fa6c3703206c2c7f611a4b4c
Seed <cb.so.devope+0001@gmail.com>
2014-11-06 15:24:59 +0900

    Create AbstractAccessStudy.java

---
+++ src/jp/com/koto/workshop/AbstractAccessStudy.java
@@ -0,0 +1,39 @@
+package hoge;
+
+abstract class AbstractAccessStudy {
+    final int superVariable = 100;
+
+    final int var = 100;
+
+    abstract protected AccessStudy accessSame();
+
+    abstract protected void accessAnother();
+
+    final protected void ln(final Number var) {
+        if (var instanceof Integer) {
+            System.out.println(var.intValue());
+        } else if (var instanceof Long) {
+            System.out.println(var.longValue());
+        } else if (var instanceof Float) {
+            System.out.println(var.floatValue());
+        } else if (var instanceof Double) {
+            System.out.println(var.doubleValue());
+        } else if (var instanceof Short) {
+            System.out.println(var.shortValue());
+        } else if (var instanceof Byte) {
+            System.out.println(var.byteValue());
+        } else {
+            throw new ClassCastException();
+        }
+    }
+
+    final protected void ln(final Object var) {
+        try {
+            System.out.println((String) var);
+        } catch (Exception e) {
+            throw new ClassCastException();
+        }
+    }
+
+    protected AbstractAccessStudy() {}
+}
