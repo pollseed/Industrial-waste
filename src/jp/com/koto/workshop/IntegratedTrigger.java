7e4b141c3630fa50b4127690fadb0c74f9f612a1
Seed <cb.so.devope+0001@gmail.com>
2014-11-06 15:53:39 +0900

    Update IntegratedTrigger.java

--- src/jp/com/koto/workshop/IntegratedTrigger.java
+++ src/jp/com/koto/workshop/IntegratedTrigger.java
@@ -1,5 +1,7 @@
 package hoge;
 
+import javax.tools.ToolProvider;
+
 /**
  * 勉強会用テストクラス(超入門編)
  *
@@ -7,9 +9,19 @@ package hoge;
 final public class IntegratedTrigger {
 
     public static void main(String[] args) {
+        new IntegratedTrigger();
+    }
 
+    private IntegratedTrigger() {
+        // コンパイラーチェック
+        this.compilerCheck();
         // アクセステスト
         new AccessStudy().accessSame().accessAnother();
+        // 測定テスト
+        new TypeConversion().convert();
+    }
 
+    private void compilerCheck() {
+        System.out.println(ToolProvider.getSystemJavaCompiler().run(null, null, null, "-version"));
     }
 }
