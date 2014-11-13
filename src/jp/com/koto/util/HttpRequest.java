e3717fa38a6b8c8c30bf5d64236407c5f038c094
Seed <cb.so.devope+0001@gmail.com>
2014-11-13 17:37:19 +0900

    Create HttpRequest.java

---
+++ src/jp/com/koto/util/HttpRequest.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.ws.http.HTTPException;
+
+public class HttpRequest extends HttpURLConnection {
+
+    protected HttpRequest(URL u, int timeout) {
+        super(u);
+        setConnectTimeout(timeout);
+    }
+
+    protected HttpRequest(URL u, int timeout, boolean doinput, boolean usecaches) {
+        this(u, timeout);
+        setDoInput(doinput);
+        setUseCaches(usecaches);
+    }
+
+    @Override
+    public void disconnect() {
+        throw new HTTPException(HTTP_BAD_GATEWAY);
+    }
+
+    @Override
+    public boolean usingProxy() {
+        throw new HTTPException(HTTP_BAD_GATEWAY);
+    }
+
+    @Override
+    public void connect() throws IOException {
+        if (super.connected) {
+            return;
+        }
+    }
+}
